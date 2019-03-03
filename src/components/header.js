fdbc16b6db885ba99bc56dce29773f22b5d83fcd
Jawad Rehman <jrmemon@gmail.com>
2019-03-03 20:57:23 +0000

    Updated header to include myself

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,26 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
 import Color from "../constants/color"
+import profilepic from "../images/profile-pic.jpg"
+
+const ProfileImage = styled.img`
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
+  margin: 0px 0px 10px 0px;
+`
+
+const ProfileImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+`
 
 const Header = ({ siteTitle }) => (
   <header
@@ -11,14 +30,16 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
+        display: "flex",
         margin: `0 auto`,
         maxWidth: "31rem",
         padding: `0.80rem 1.0875rem`,
         marginRight: "auto",
         marginLeft: "auto",
+        alignItems: "center",
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0, flex: 1 }}>
         <Link
           to="/"
           style={{
@@ -28,6 +49,10 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <ProfileImageContainer>
+        <ProfileImage src={profilepic} alt="Profile Picture" />
+        <h5>Jawad Rehman</h5>
+      </ProfileImageContainer>
     </div>
   </header>
 )
